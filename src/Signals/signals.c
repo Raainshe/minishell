e2d3d6af287ab5b69fe04a59e2dd09b56a0fcfc1
Raain <ryanbwgt@gmail.com>
2025-04-22 13:12:07 +0200

    comments for functions on how they work

--- src/Signals/signals.c
+++ src/Signals/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 12:26:06 by ksinn             #+#    #+#             */
-/*   Updated: 2025/04/15 12:49:55 by ksinn            ###   ########.fr       */
+/*   Updated: 2025/04/22 12:52:06 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 volatile sig_atomic_t	g_signal_received = 0;
 
+/**
+ * @brief Handles SIGINT signal in interactive mode
+ * @param signum The signal number (always SIGINT)
+ *
+ * This function sets the global signal flag, writes a newline to stderr,
+ * and resets the readline state to prepare for new input.
+ */
 static void	handle_sigint_interactive(int signum)
 {
 	g_signal_received = signum;
@@ -23,18 +30,37 @@ static void	handle_sigint_interactive(int signum)
 	rl_redisplay();
 }
 
+/**
+ * @brief Handles SIGINT signal in non-interactive mode
+ * @param signum The signal number (always SIGINT)
+ *
+ * This function sets the global signal flag and writes a newline to stderr.
+ */
 static void	handle_sigint_noninteractive(int signum)
 {
 	g_signal_received = signum;
 	write(STDERR_FILENO, "\n", 1);
 }
 
+/**
+ * @brief Handles SIGQUIT signal in non-interactive mode
+ * @param signum The signal number (always SIGQUIT)
+ *
+ * This function sets the global signal flag and writes a message to stderr
+ * indicating a core dump.
+ */
 static void	handle_sigquit_noninteractive(int signum)
 {
 	g_signal_received = signum;
 	write(STDERR_FILENO, "Quit (core dumped)\n", 19);
 }
 
+/**
+ * @brief Sets up signal handlers for interactive mode
+ *
+ * This function configures SIGINT to be handled by handle_sigint_interactive
+ * and ignores SIGQUIT.
+ */
 void	setup_interactive_signals(void)
 {
 	struct sigaction	sa_int;
@@ -50,6 +76,12 @@ void	setup_interactive_signals(void)
 	sigaction(SIGQUIT, &sa_quit, NULL);
 }
 
+/**
+ * @brief Sets up signal handlers for non-interactive mode
+ *
+ * This function configures SIGINT to be handled by handle_sigint_noninteractive
+ * and SIGQUIT to be handled by handle_sigquit_noninteractive.
+ */
 void	setup_noninteractive_signals(void)
 {
 	struct sigaction	sa_int;
@@ -65,12 +97,24 @@ void	setup_noninteractive_signals(void)
 	sigaction(SIGQUIT, &sa_quit, NULL);
 }
 
+/**
+ * @brief Resets signal handlers to their default behavior
+ *
+ * This function restores the default handlers for SIGINT and SIGQUIT.
+ */
 void	reset_signals(void)
 {
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 }
 
+/**
+ * @brief Retrieves the status code based on the last received signal
+ * @return The status code: 130 for SIGINT, 131 for SIGQUIT, or 0 otherwise
+ *
+ * This function checks the global signal flag, determines the appropriate
+ * status code, and resets the flag.
+ */
 int	get_signal_status(void)
 {
 	int	status;
