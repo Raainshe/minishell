dbe325b78bb7c4fbebbdf01433b949d99439a6b7
Raain <ryanbwgt@gmail.com>
2025-04-28 14:40:21 +0200

    environ fix

--- src/environ.c
+++ src/environ.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   environ.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 14:36:51 by ksinn             #+#    #+#             */
-/*   Updated: 2025/04/25 13:05:11 by ksinn            ###   ########.fr       */
+/*   Updated: 2025/04/28 14:35:36 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,11 +60,39 @@ static char	*ft_increment_shlvl(char *shlvl_str)
 	return (new_shlvl);
 }
 
+/**
+ * @brief Add an environment variable to the environment list
+ * @param env The environment list to add to
+ * @param env_var The environment variable string to add
+ * @return Updated environment list
+ */
+static t_list	*add_env_var(t_list **env, char *env_var)
+{
+	char	*env_copy;
+	t_list	*tmp;
+
+	if (strncmp(env_var, "SHLVL=", 6) == 0)
+		env_copy = ft_increment_shlvl(env_var);
+	else
+		env_copy = ft_strdup(env_var);
+	if (!env_copy)
+		return (*env);
+	gc_add_context(ENVIRON, env_copy);
+	tmp = ft_lstnew(env_copy);
+	gc_add_context(ENVIRON, tmp);
+	ft_lstadd_back(env, tmp);
+	return (*env);
+}
+
+/**
+ * @brief Copy the environment variables into a linked list
+ * @param environ Array of environment variables
+ * @return Linked list containing the environment variables
+ */
 t_list	*copy_environ(char **environ)
 {
 	int		i;
 	t_list	*env;
-	t_list	*tmp;
 	char	*env_copy;
 
 	if (!environ || !environ[0])
@@ -78,16 +106,7 @@ t_list	*copy_environ(char **environ)
 	i = 1;
 	while (environ[i])
 	{
-		if (strncmp(environ[i], "SHLVL=", 6) == 0)
-			env_copy = ft_increment_shlvl(environ[i]);
-		else
-			env_copy = ft_strdup(environ[i]);
-		if (!env_copy)
-			return (env);
-		gc_add_context(ENVIRON, env_copy);
-		tmp = ft_lstnew(env_copy);
-		gc_add_context(ENVIRON, tmp);
-		ft_lstadd_back(&env, tmp);
+		add_env_var(&env, environ[i]);
 		i++;
 	}
 	return (env);
