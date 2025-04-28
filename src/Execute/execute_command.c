1b7753941505d0646e6c2234278fb5c664af10a8
Raain <ryanbwgt@gmail.com>
2025-04-28 14:20:24 +0200

    tokenize norm fix

--- src/Execute/execute_command.c
+++ src/Execute/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 14:46:48 by ksinn             #+#    #+#             */
-/*   Updated: 2025/04/27 11:26:20 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/04/28 14:05:42 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,45 +124,45 @@ static int	execute_builtin(char **args, t_list **env)
 	return (0);
 }
 
-int	execute_command(t_node *node, t_list **env)
+/**
+ * @brief Function to handle the child process execution
+ * @param command The command to execute
+ * @param env The environment variables
+ */
+static void	execute_child_process(t_command *command, t_list *env)
 {
-	t_command	*command;
-	pid_t		pid;
-	int			status;
-	char		*path;
-	char		**env_array;
+	char	*path;
+	char	**env_array;
 
-	if (!node || !node->data)
-		return (1);
-	command = (t_command *)node->data;
-	if (!command->args || !command->args[0])
-		return (0);
-	if (is_builtin(command->args[0]))
-		return (execute_builtin(command->args, env));
-	pid = fork();
-	if (pid < 0)
-		return (perror("minishell: fork"), 1);
-	if (pid == 0)
+	reset_signals();
+	path = find_command_path(command->args[0], env);
+	if (!path)
 	{
-		reset_signals();
-		path = find_command_path(command->args[0], *env);
-		if (!path)
-		{
-			ft_putstr_fd("minishell: ", STDERR_FILENO);
-			ft_putstr_fd(command->args[0], STDERR_FILENO);
-			ft_putendl_fd(": command not found", STDERR_FILENO);
-			exit(127);
-		}
-		env_array = convert_env_to_array(*env);
-		if (!env_array)
-		{
-			perror("minishell: malloc");
-			exit(1);
-		}
-		execve(path, command->args, env_array);
-		perror("minishell: execve");
-		exit(126);
+		ft_putstr_fd("minishell: ", STDERR_FILENO);
+		ft_putstr_fd(command->args[0], STDERR_FILENO);
+		ft_putendl_fd(": command not found", STDERR_FILENO);
+		exit(127);
 	}
+	env_array = convert_env_to_array(env);
+	if (!env_array)
+	{
+		perror("minishell: malloc");
+		exit(1);
+	}
+	execve(path, command->args, env_array);
+	perror("minishell: execve");
+	exit(126);
+}
+
+/**
+ * @brief Handle the parent process after forking
+ * @param pid The child process ID
+ * @return Exit status of the child process
+ */
+static int	handle_parent_process(pid_t pid)
+{
+	int	status;
+
 	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
@@ -175,4 +175,30 @@ int	execute_command(t_node *node, t_list **env)
 		return (128 + WTERMSIG(status));
 	}
 	return (1);
+}
+
+/**
+ * @brief Execute a command, either builtin or external
+ * @param node The command node
+ * @param env The environment variables
+ * @return Exit status of the command
+ */
+int	execute_command(t_node *node, t_list **env)
+{
+	t_command	*command;
+	pid_t		pid;
+
+	if (!node || !node->data)
+		return (1);
+	command = (t_command *)node->data;
+	if (!command->args || !command->args[0])
+		return (0);
+	if (is_builtin(command->args[0]))
+		return (execute_builtin(command->args, env));
+	pid = fork();
+	if (pid < 0)
+		return (perror("minishell: fork"), 1);
+	if (pid == 0)
+		execute_child_process(command, *env);
+	return (handle_parent_process(pid));
 }
